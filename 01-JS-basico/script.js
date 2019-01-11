26a31ae0566340b2b35fbf248bcd9f1b0242519c
“salmuzblues” <salmuzblues@gmail.com>
2019-01-11 18:31:03 -0500

    variables javascript

---
+++ 01-JS-basico/script.js
@@ -0,0 +1,8 @@
+//console.log('Hello Alex !!!!!');
+
+var name = 'Juan';
+console.log(`His name is ${ name }`);
+var lastName = 'Carranza';
+var age = 36;
+var fullAge = true;
+console.log(`He is ${ age } years old and is ${ fullAge }`);
