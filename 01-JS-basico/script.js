071a63a9e97499510d4d2fcad2235d4ad633c3f4
“salmuzblues” <salmuzblues@gmail.com>
2019-01-11 19:01:20 -0500

    functions like alert and prompt

--- 01-JS-basico/script.js
+++ 01-JS-basico/script.js
@@ -1,8 +1,17 @@
 //console.log('Hello Alex !!!!!');
-
+/* variable Mutation and type coercion */
 var name = 'Juan';
 console.log(`His name is ${ name }`);
 var lastName = 'Carranza';
 var age = 36;
 var fullAge = true;
 console.log(`He is ${ age } years old and is ${ fullAge }`);
+// Anoter way to decalre a variable 
+var job, ismarried;
+job = 'Musician';
+ismarried = false;
+// this is show a  short windows for message. alert();
+alert(`Joe is a ${ job } and he is not married ${ ismarried }`);
+// prompt is like a input .
+var lastName = prompt('What is his lastname');
+console.log(name + ' ' + lastName);
