21f52939f32189a6f73b521c68eae5fe076ba365
“salmuzblues” <salmuzblues@gmail.com>
2019-01-14 18:44:56 -0500

    game ... pig game

--- 03-DOM-pig-game/app.js
+++ 03-DOM-pig-game/app.js
@@ -8,3 +8,66 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
+// create variables 
+var scores = [0, 0];
+var roundScore = 0;
+var acitvePlayer = 0;
+
+// manipulanto el DOM .. 
+// this change the number current
+// document.querySelector('#current-' + acitvePlayer).textContent = dice;
+
+// // this change the html context 
+// //document.querySelector('#current-' + acitvePlayer).innerHTML = '<em>' + dice + '</em>';
+// // jus for reading or retreiving some information from html. 
+
+// var x = document.querySelector('#score-1').textContent;
+// console.log(x);
+
+// how to change css styles by javaScript 
+document.querySelector('.dice').style.display = 'none';
+// this is another way and it is faster than .querySelector, and 
+// this is because we work by ID  NOT a class 
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+// first we need to select the tag of html that we need to work. 
+document.querySelector('.btn-roll').addEventListener('click', () => {
+    // 1.Random Number.
+    var dice = Math.floor(Math.random() * 6) + 1;
+    // 2. Display the result 
+    var diceDom = document.querySelector('.dice'); // this is for select the class CSS;
+    // block in css: Displays an element as a block element (like <p>).
+    // It starts on a new line, and takes up the whole width 
+    diceDom.style.display = 'block';
+    diceDom.src = 'dice-' + dice + '.png';
+    // 3.- Update the round score if the rolled number was not a 1. 
+    if (dice !== 1) {
+        // add score points
+        roundScore += dice;
+        document.querySelector('#current-' + acitvePlayer).textContent = roundScore;
+
+    } else {
+        // next player// changing the player 
+        acitvePlayer === 0 ? acitvePlayer = 1 : acitvePlayer = 0;
+        roundScore = 0;
+        // vamos a settear 
+        document.getElementById('current-0').textContent = '0';
+        document.getElementById('current-1').textContent = '0';
+
+        // remove the class .active 
+        document.querySelector('.player-0-panel').classList.toggle('active');
+        // adding for another tag the class .active 
+        document.querySelector('.player-1-panel').classList.toggle('active');
+
+        document.querySelector('.dice').style.display = 'none';
+
+        /** JUST FOR REFERENCES  */
+        // remove the class .active 
+        // document.querySelector('.player-0-panel').classList.remove('active'); 
+        // adding for another tag the class .active 
+        //document.querySelector('.player-1-panel').classList.add('active'); 
+    }
+
+});
