e56301c4747b7117cfd42065fc2eeb69f26c71a2
salmuzblues <salmuzblues@gmail.com>
2019-05-31 17:00:27 -0500

    Passing functions as Argument

--- 04-Advance-js/script.js
+++ 04-Advance-js/script.js
@@ -1,12 +1,12 @@
 
 // javascript Object 
 
- var john = {
+/*  var john = {
     name: 'jhon', 
     yearOfBirth: 1990,
     job: 'teacher' 
  }; 
-
+ */
 //Function constructor 
 // this is like a create a class with its constructor.
 // Person is the class function as constructor
@@ -26,13 +26,59 @@
   }; 
   
   Person.prototype.hobby = "Listening music"; 
-  /* END PTOTOTYPE  */
+  // END PTOTOTYPE  
 
   var Mike = new Person('Mike', 1985, 'Designer'); 
   var Rosa = new Person('Rosa', 1982, 'translater'); 
 
-  Mike.calculateAge(); 
+/*   Mike.calculateAge(); 
   Rosa.calculateAge(); 
   console.log(Mike.name + ' loves ' + Mike.hobby); 
-  console.log(Rosa.name + ' loves ' + Rosa.hobby); 
-  
+  console.log(Rosa.name + ' loves ' + Rosa.hobby);  */
+
+  // Object.creat takes personProp Object and you can use for many object 
+  //that you can create 
+ var personProp = {
+    calculateAge: function(){
+        console.log(2019 - this.yearOfBirth); 
+    }
+ }
+
+  // Object.create   there is one way
+  var Jhon = Object.create(personProp); 
+
+  Jhon.name = 'Jhon'; 
+  Jhon.lastName = 'Smith'; 
+  Jhon.yearOfBirth = 1982; 
+  //Jhon.calculateAge(); 
+
+  // Object.create   there is another way
+
+  var Rosario = Object.create(personProp, 
+    {
+        name: { value: 'Rosario' },
+        yearOfBirth: { value: 1969 }, 
+        job: { value: 'Designer'}    
+    }); 
+
+
+    // Primitives  
+    var a = 23; 
+    var b = a; 
+    a = 46; 
+    console.log(a); 
+    console.log(b); 
+
+    //Objects
+
+    var obj1 = {
+        name: 'Jhon',
+        age: 24
+    }; 
+    var obj2 = obj1; 
+
+    console.log(obj1.age); 
+    console.log(obj2.age); 
+
+ // Objects  
+
