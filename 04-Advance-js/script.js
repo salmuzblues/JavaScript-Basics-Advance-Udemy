815ad97b33194dade7d867e2d02d4f66a5559124
salmuzblues <salmuzblues@gmail.com>
2019-06-24 13:04:19 -0500

    functions call, bind and apply

--- 04-Advance-js/script.js
+++ 04-Advance-js/script.js
@@ -108,7 +108,7 @@
 
  /* Passing functions as arguments */
 
- var years = [1990, 1982, 1937, 2005, 1998]; 
+ /* var years = [1990, 1982, 1937, 2005, 1998]; 
 
 
 // main function for storing the results into a new array
@@ -142,7 +142,7 @@
   console.log("array of ages: " + ages);
   console.log("Array fullAges more than 18: " + fullAges); 
   console.log("RATES HEART: " + rates); 
-
+ */
 
   /* Functions returning functions */
   function interviewQuestion(job){
@@ -170,7 +170,7 @@
   // Other way , the another parenthesis  is for then anonimous function
   interviewQuestion('teacher')('Yonatan'); 
 
-/*  IIFE  pattern */
+/*  IIFE  pattern (immediately Invoke Function Expressions) */
 // Declaration function 
 
 /* function game () {
@@ -185,4 +185,135 @@
   var score = Math.random() * 10;  // 0 - 9 
   console.log(score >= 5 - goodLuck);
  })(4); 
+  
+ /* CONCEPTS CLOSURES  */
 
+ /* An inner function has always access to the variables and parameters of its outer function,
+ even after the outer  function has return  */
+
+ function retirement(retAge){
+   var a = ' years left until retirement.'; 
+   return function (yearOfBirth) {
+     var age = 2019 - yearOfBirth; 
+     console.log((retAge - age) + a); 
+   }
+ }
+
+  var retirementUs = retirement(66); 
+  var retirementGerm = retirement(65); 
+  var retirementIceland = retirement(67); 
+
+  // Results  insert years of person 
+  retirementUs(1982); 
+  retirementGerm(1990); 
+  retirementIceland(1986); 
+  //retirement(66)(1985); 
+
+
+ /*  function interviewQuestion(job){
+    if (job === 'designer'){
+      return function (name){
+        console.log( name + ', can you please explain what UX design is?'); 
+      }
+    }else if(job === 'teacher'){
+      return function(name){
+        console.log('What subject do you teach, ' + name + '?')}
+    }else{
+      return function(name){
+        console.log('Hello ' + name + ', what do you do? ')
+      }
+    }
+  } */
+    
+/*   function interviewQuestionClosures(job){
+
+    return function(name){
+       if(job === 'designer'){
+        var a = ', can you please explain what UX design is?'; 
+        console.log(name + a);   
+      }else if( job === 'teacher'){
+        var b = 'What subject do you teach ' + name + '?'
+         console.log(b); 
+      }else {
+        console.log('Hello ' + name + ', what do you do'); 
+      }
+    }
+        
+  }
+
+  var respDesigner = interviewQuestionClosures('designer'); 
+  respDesigner('Yonatan'); 
+  interviewQuestion('teacher')('Richard');  */
+
+  /* BIND, CALL  AND APPLY  */
+
+  var jhon = {
+    name: 'Jhon', 
+    age: 26, 
+    job: 'teacher', 
+    presentation: function(style, timeOfDay){
+      if(style === 'formal'){
+      console.log('Good ' + timeOfDay + ', Ladies and gentlemen! I\'m ' + 
+                    this.name + ', I\'m a'+
+                    this.job + ' and I\'m '+
+                    this.age + ' years old.');  
+      }else if(style === 'friendly'){
+        console.log('Hey! What\'s up? I\'m ' +
+                    this.name + ', I\'m a '+
+                    this.job + ' and I\'m '+
+                    this.age + ' years old. Have a nice ' +
+                    timeOfDay + '.');  
+      }
+    }
+  }
+
+  jhon.presentation('formal', 'morning'); 
+
+  var emily = {
+    name: 'Emily', 
+    age: 35, 
+    job: 'designer'
+  }; 
+
+  // Using Jhon's function presentation in Emily´s Object
+  // this call() borrows the function 
+  jhon.presentation.call(emily, 'friendly', 'afternoon'); 
+  // apply() works with array arguments.  
+  /* jhon.presentation.apply(emily, [], []);  */
+  
+  /* Bind is very similar to the call method as well, so it is also to allow us to set  this variable ixplicity 
+      THE BIND doesn't immediately call the function but instead it generates  a copy of the function. so tha we can store it somewhere */
+  var jhonFriendly = jhon.presentation.bind(jhon, 'friendly'); 
+
+   jhonFriendly('morning'); 
+   jhonFriendly('night'); 
+
+   var emilyFormal = jhon.presentation.bind(emily, 'formal'); 
+
+   emilyFormal('afternoon');   
+
+
+
+   var years = [1990, 1982, 1937, 2005, 1998]; 
+
+   // main function for storing the results into a new array
+     function arrayCalc (arr, fn ){
+       var arrRes = []; 
+        for( var i = 0; i < arr.length; i++){
+           arrRes.push(fn(arr[i])); 
+        } 
+        return arrRes; 
+     }
+   
+     // Create a  functions to calculate the age
+     function calculateAge(el){ 
+       return 2019 - el;  
+     }
+     function isFullAge(limit, el){
+        return el >= limit; 
+     }
+
+     var ages = arrayCalc(years, calculateAge); 
+     var fullJapan = arrayCalc(ages, isFullAge.bind(this, 20)); 
+     console.log(ages); 
+     console.log(fullJapan); 
