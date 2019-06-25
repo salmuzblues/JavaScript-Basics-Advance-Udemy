6c80656ebaf2683991cfb0d00855e3b230266609
salmuzblues <salmuzblues@gmail.com>
2019-06-25 14:07:25 -0500

    finished the basic challenged

--- 04-Advance-js/challengePractice.js
+++ 04-Advance-js/challengePractice.js
@@ -20,9 +20,9 @@ c) correct answer (I would use a number for this)
 
 7. Suppose this code would be a plugin for other programmers to use in their code. So make sure that all your code is private and doesn't interfere with the other programmers code (Hint: we learned a special technique to do exactly that).
 */
-
+var chooseQuestion = Math.floor(Math.random() * 3); 
  var question = function (){
-  var chooseQuestion = Math.floor(Math.random() * 3); 
+
     if(chooseQuestion === 0 ){
         question1();    
     }else if(chooseQuestion === 1){
@@ -33,24 +33,51 @@ c) correct answer (I would use a number for this)
 }; 
 
   question(); 
+  var result = prompt("which is the correct answer? "); 
+   
+  if (chooseQuestion === 0){
+    if (result === '0' ){
+        console.log("Incorrect Answer"); 
+    }else if (result === '1') {
+        console.log("Correct Answer")
+    }else {
+        console.log("Wrong Answer"); 
+    }   
+ }else if(chooseQuestion === 1){
+    if (result === '0' ){
+        console.log("Incorrect Answer"); 
+    }else if (result === '1') {
+        console.log("Correct Answer")
+    }else if (result === '0'){
+        console.log("Incorrect Answer"); 
+    }else {
+        console.log("Wrong Answer"); 
+    }  
+ }else if (chooseQuestion === 2){
+    if (result === '1' ){
+        console.log("Incorrect Answer"); 
+    }else if (result === '0') {
+        console.log("Correct Answer")
+    }else {
+        console.log("Wrong Answer"); 
+    } 
+ }
 
  function question1(number){
     console.log("Is the JavaScript Languaje the coolest?");
     console.log("0. No ");
     console.log("1. Yes "); 
-    return function(){
-        if (number === 0 ){
-            console.log("Incorrect Answer"); 
-        }else if (number === 1) {
-            console.log("Correct Answer")
-        }   
-    }
  }
 
  function question2 (){
     console.log("What is your  name ? ");
+    console.log("0. Jhon");
+    console.log("1. Alex");
+    console.log("2. Ricardo");
  }
  function question3 (){
     console.log("Do you love coding?");
+    console.log("0. Yes of course ");
+    console.log("1. No");
  }
  
